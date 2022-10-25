beadaa820c099bf2d97644cd6d4c69495127be7a
tonny187 <tonyoanthony@gmail.com>
2022-10-25 11:45:56 -0500

    adding concepts definitions

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -26,7 +26,7 @@ const Services = () => {
           </div>
 
           <ul className={styles.service_list}>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor expedita optio voluptatibus odio in neque voluptatum nisi ad. Ut dicta libero illum ab optio ratione veniam nostrum quae neque natus!s</p>
+          <p> Se trabaja en base al Manual Único para la Calificación de la Pérdida de la Capacidad Laboral y Ocupacional. Decreto 1507 de 2014, En el que procedemos como primer paso la definición del daño estructural, para luego definir la repercusión BioPsicosocial. Sus etapas consisten en: Evaluación incial, análisis de la información, evaluación integral y entrega del informe y/o calificación</p>
           </ul>
         </article>
 
@@ -39,7 +39,7 @@ const Services = () => {
           </div>
 
           <ul className={styles["service_list"]}>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor expedita optio voluptatibus odio in neque voluptatum nisi ad. Ut dicta libero illum ab optio ratione veniam nostrum quae neque natus!s</p>
+          <p>Basado en el derecho civil, y siempre de la mano de un abogado, este servicio busca investigar, determinar y emitir un perito médico que soporte la causalidad o los hechos que afectaron la salud de una persona a corto o largo plazo, dando la apertura de un proceso legal, tal como una mala práctica o negligencia médicos por ejemplo. </p>
           </ul>
         </article>
 
@@ -52,7 +52,7 @@ const Services = () => {
           </div>
 
           <ul className={styles["service_list"]}>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor expedita optio voluptatibus odio in neque voluptatum nisi ad. Ut dicta libero illum ab optio ratione veniam nostrum quae neque natus!s</p>
+          <p>Contamos con los mejores profesionales a tu servicio, para ofrecerte una guía basada en el conocimiento técnico y juridico en la interpretación de dictamenes médicos o culaquier situación de esta índole, dividido por etapas, buscamos tener un proceso ágil y efectivo direccionado a tu proposito.</p>
           </ul>
         </article>
 
@@ -78,7 +78,7 @@ const Services = () => {
           </div>
 
           <ul className={styles["service_list"]}>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor expedita optio voluptatibus odio in neque voluptatum nisi ad. Ut dicta libero illum ab optio ratione veniam nostrum quae neque natus!s</p>
+              <p>Una vez que recibes una calificación por parte de alguna entidad tal como la EPS, el ARL, Fondo de Pensiones, Junta Regional, etc. Puedes hacer una apelación de "no conformidad" Este servicio cumple con la resolución 1239 del 2022 emitida por la secretaría de salud.</p>
           </ul>
         </article>
       </div>
