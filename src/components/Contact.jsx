f61d9eb7987f93bba324218f59dfa0bc80ac6d1c
tonny187 <tonyoanthony@gmail.com>
2022-12-13 18:29:08 -0500

    adding animation in the contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -72,7 +72,7 @@ const Contact = () => {
               </a>
             </article>
           </div>
-          <form className={styles["form"]} ref={form}
+          {/* <form className={styles["form"]} ref={form}
           onSubmit={sendEmail}>
             <input
               className={styles.input}
@@ -105,7 +105,23 @@ const Contact = () => {
             <button type="submit" className={styles.contactButton}>
               Enviar
             </button>
-          </form>
+          </form> */}
+
+        <div className={styles.box2}>
+        <form ref={form} onSubmit={sendEmail} className={styles.form2}>
+            <h2>Envíanos tu mensaje</h2>
+            <div className={styles.inputBox}>
+                <input type="text" name="name" placeholder='Nombre y Apellido' required="required" />
+                <i></i>
+            </div>
+            <div className={styles.inputBox}>
+                <input type="email" name='email' placeholder='Correo Electrónico' required="required" />
+                <i></i>
+            </div>
+          <textarea className={styles.inputBox} name="message" placeholder='Necesidad del Usuario' rows="7" required></textarea>
+          <button type='submit' className={styles.btnPrimary1}>Enviar</button>
+        </form>
+    </div>
         </div>
       </div>
     </section>
